86b410cd04bf00f95d298b8d031cb16745fb837c
Justin Caldwell <justinwritescode@gmail.com>
2016-07-26 18:43:14 -0600

    Migration of authentiation files and HTML and CSS components.  Reorganization of file structure.

---
+++ app/config/config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Dependencies
+//var secret = require('./jwt.config');
+//var uri = require('./db.config');
+
+module.exports = {
+    server: {
+        port: process.env.PORT || 3000
+    },
+    jwt: {
+        secret: 'topSecretSecretForMe'
+    },
+    db: {
+        uri: 'mongodb://localhost/auth'
+    }
+};
